a733a2b135ed073348440d25bbf08fc2ad7b76b6
Wolfgang <wolfgang.romanowski0@gmail.com>
2025-11-09 22:30:40 +0000

    Initial commit

---
+++ test/ca1_cloud_dev.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ca1CloudDev from '../lib/ca1_cloud_dev-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ca1_cloud_dev-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ca1CloudDev.Ca1CloudDevStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
